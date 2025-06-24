73cce02ba1306a3396bcd1eac7246c6a596d431e
anshumann01 <155174498+anshumann01@users.noreply.github.com>
2025-06-24 11:17:16 +0000

    Conversion of data type

--- test.js
+++ test.js
@@ -1,1 +1,22 @@
 console.log("Hello");
+console.log(typeof "Hello");
+
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+let num = "123";
+console.log(typeof num);
+
+let valueInNumber = Number(num);
+console.log(typeof valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(typeof booleanIsLoggedIn);
+
+// 1 => true 0 => false
+// "" => false "Anshuman" => true
