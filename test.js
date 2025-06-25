d9a2f9717abb491ed21e715b89a5cc6064fa3416
anshumann01 <155174498+anshumann01@users.noreply.github.com>
2025-06-25 07:06:34 +0000

    comparison of data types

--- test.js
+++ test.js
@@ -20,3 +20,15 @@ console.log(typeof booleanIsLoggedIn);
 
 // 1 => true 0 => false
 // "" => false "Anshuman" => true
+
+// console.log("2">1); // true
+
+// console.log(null>0) //false
+// console.log(null>=0) //true
+// console.log(null==0) //false
+
+// console.log(undefined==0) //false
+// console.log(undefined < 0) //false
+// console.log(undefined>0) //false
+
+// console.log("2"===2) //false
