04bf2f880baf64260e4a64177a9b29a8533fc542
anshumann01 <155174498+anshumann01@users.noreply.github.com>
2025-06-25 12:57:03 +0000

    data types summary

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
-console.log("Hello");
-console.log(typeof "Hello");
+// console.log("Hello");
+// console.log(typeof "Hello");
 
 // console.log(typeof null);
 // console.log(typeof undefined);
 
-let num = "123";
-console.log(typeof num);
+// let num = "123";
+// console.log(typeof num);
 
-let valueInNumber = Number(num);
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(num);
+// console.log(typeof valueInNumber);
 
-//"33" => 33
-//"33abc" => NaN
+// //"33" => 33
+// //"33abc" => NaN
 
-let isLoggedIn = 1;
-let booleanIsLoggedIn = Boolean(isLoggedIn);
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(typeof booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
 
 // 1 => true 0 => false
 // "" => false "Anshuman" => true
@@ -32,3 +32,23 @@ console.log(typeof booleanIsLoggedIn);
 // console.log(undefined>0) //false
 
 // console.log("2"===2) //false
+
+//Data Types
+//primitive data types - 7 types (call by value)
+//String, Number, boolean, null, undefined, Symbol, BigInt
+
+//non-primitive/reference type (call by reference)
+//Arrays, Objects, Functions
+
+// const Id = Symbol('123');
+// const anotherId = Symbol('123');
+
+// console.log(Id==anotherId) //false
+
+// const myfunction = function() {
+//     console.log("hello world")
+// }
+// console.log(typeof myfunction) //function
+
+// const number  = 238563297562379562n
+// console.log(typeof number) //bigint
