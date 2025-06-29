263cea3d470eccbcea5d00a08dc5dc1868d63e29
anshumann01 <155174498+anshumann01@users.noreply.github.com>
2025-06-29 08:03:30 +0000

    this and arrow function

--- test.js
+++ test.js
@@ -291,6 +291,88 @@ function handleArray(anyArray) {
 }
 
 // console.log(handleArray(arr)); //200
- 
 
+// ******************************************* Scopes in JS ***************************************************
+
+if(true) {
+    const a = 10
+    let b = 20
+    var c = 30
+}
+
+// console.log(a); //ReferenceError: a is not defined
+// console.log(b); //ReferenceError: b is not defined
+// console.log(c); //30 - var does not follow scope rules that's why we do not prefer using var 
+
+ //expression
+//  const addTwo = function(num) {
+//     return num+2;
+//  }
+
+// console.log(addTwo(5)); //7 - In function we can access our function above the function written , but here we have to first write our expression then we can access it.
+
+// *********************************************** This and Arrow Function ***********************************************************
+
+// function printName() {
+//     const username = "Anshuman";
+//     console.log(`Hello ${this.username} welcome to the website`);
+// }
+// printName() //Hello undefined welcome to the website
+
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(username);
+    
+    console.log(this);
+}
+
+
+chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2 //implicit return
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+// const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+// console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
 
